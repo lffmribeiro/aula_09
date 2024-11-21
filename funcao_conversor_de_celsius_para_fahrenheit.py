d2cd10dc97ea61594f093be50d9aa3022bf55cd8
Luiz Fernando Ribeiro <fernandolmessias@hotmail.com>
2024-11-21 11:36:49 -0300

    modificando a funcao converter e o requirements

--- funcao_conversor_de_celsius_para_fahrenheit.py
+++ funcao_conversor_de_celsius_para_fahrenheit.py
@@ -7,12 +7,13 @@ lista_celsius: float = []
 
 @validate_call
 def c_para_f(lista_celsius):
+    '''Essa funcao recebe uma lista como parâmetro e converte Celsius em Fahrenheit'''
     lista_fahrenheit: float = []
     for c in lista_celsius:
         f = (c* 9/5) + 32
         lista_fahrenheit.append(f)
     return lista_fahrenheit
 
-lista_celsius = [0,1,20.3,45.5]
+lista_celsius = [0,1,20.3,45.5,"a"]
 convercao = c_para_f(lista_celsius)
 print(convercao)
